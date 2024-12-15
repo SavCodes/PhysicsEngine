30c3477e691738aa79444386dabcc9374420663f
SavCodes <91900522+SavCodes@users.noreply.github.com>
2024-12-15 17:41:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ def resolve_y_collision(object, wall):
     # Upwards collision detection
     if wall.is_collidable and object.position[1] < wall.position[1] + wall.height and object.position[1] + object.height > wall.position[1] + wall.height:
         object.position[1] = wall.position[1] + wall.height
+
+# Resolves horizontal collisions between two rectangular objects
+def resolve_x_collision(object, wall):
+    # Right-sided collision detection
+    if wall.is_collidable and wall.position[0] < object.position[0] + object.width and wall.position[0] > object.position[0]:
+        object.position[0] = wall.position[0] - object.width
+        object.is_touching_ground = True
+
+    # Left-sided collision detection
+    if wall.is_collidable and object.position[0] < wall.position[0] + wall.width and object.position[0] + object.width > wall.position[0] + wall.width:
+        object.position[0] = wall.position[0] + wall.width
